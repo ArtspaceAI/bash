8f040cadb5797d6127db60d3b3333c7c1c89b39d
ArtspaceAI <151873916+ArtspaceAI@users.noreply.github.com>
2024-09-02 13:25:39 +0300

    Update lady-gaga2.sh

--- lady-gaga2.sh
+++ lady-gaga2.sh
@@ -3,48 +3,51 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-echo "Changing to the root directory..."
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+echo -e "${GREEN}Changing to the root directory...${NC}"
 # Make sure it's the root directory first
 cd ..
 
-echo "Updating packages..."
+echo -e "${GREEN}Updating packages...${NC}"
 # Update packages and install necessary tools
 sudo apt-get update -y
 sudo apt-get -y install curl tar ca-certificates screen
 
-echo "Checking if apphub is already downloaded..."
+echo -e "${GREEN}Checking if apphub is already downloaded...${NC}"
 # Download, extract, and set up the application
 if [ ! -f ../apphub-linux-amd64/apphub ]; then
-  echo "Downloading apphub..."
+  echo -e "${GREEN}Downloading apphub...${NC}"
   curl -o apphub-linux-amd64.tar.gz https://assets.coreservice.io/public/package/60/app-market-gaga-pro/1.0.4/app-market-gaga-pro-1_0_4.tar.gz
-  echo "Extracting apphub..."
+  echo -e "${GREEN}Extracting apphub...${NC}"
   tar -zxf apphub-linux-amd64.tar.gz
-  echo "Cleaning up downloaded tar file..."
+  echo -e "${GREEN}Cleaning up downloaded tar file...${NC}"
   rm -f apphub-linux-amd64.tar.gz
 fi
 
-echo "Starting apphub in a detached screen session..."
+echo -e "${GREEN}Starting apphub in a detached screen session...${NC}"
 # Start apphub in a detached screen session
 screen -S apphub-session -d -m sudo ../apphub-linux-amd64/apphub
 
-echo "Waiting for the process to start up..."
+echo -e "${GREEN}Waiting for the process to start up...${NC}"
 # Wait for the process to start up (adjust as needed)
 sleep 4
 
-echo "Starting a new screen session for status check..."
+echo -e "${GREEN}Starting a new screen session for status check...${NC}"
 # Start a new screen session for status check
 screen -S status-check -d -m bash -c '../apphub-linux-amd64/apphub status; exec bash'
 
-echo "Configuring gaganode..."
+echo -e "${GREEN}Configuring gaganode...${NC}"
 # Configure gaganode
 sudo ./apps/gaganode/gaganode config set --token=zzpozuelztsyrtbc3ce802710170d4d3
 
-echo "Restarting apphub..."
+echo -e "${GREEN}Restarting apphub...${NC}"
 # Restart apphub
 ../apphub-linux-amd64/apphub restart
 
-echo "Checking the status of the apphub again..."
+echo -e "${GREEN}Checking the status of the apphub again...${NC}"
 # Check the status of the apphub again
 ../apphub-linux-amd64/apphub status
 
-echo "Script execution completed."
+echo -e "${GREEN}Script execution completed.${NC}"
